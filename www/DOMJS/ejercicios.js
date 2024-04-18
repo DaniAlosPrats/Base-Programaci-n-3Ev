6c81bb6513423fc84376d07f17cebd566314ee8d
Dani Alos <alospratsdani@gmail.com>
2024-04-18 10:23:00 +0200

    aa

--- www/DOMJS/ejercicios.js
+++ www/DOMJS/ejercicios.js
@@ -98,9 +98,33 @@ if (ele.innerHTML === "Mostrar") {
 }
 
 function hora(){
-    let hora = "00:00:00";
-    
+    let reloj = document.createElement("div");
+    reloj.innerHTML = "00:00:00";
+    reloj.id = "reloj";
+    reloj.setAttribute("style", "position: absolute; display: none; background-color: red; color: white; border: 1px solid black;");
+    document.body.appendChild(reloj);
+
+    let parrafos = document.getElementsByTagName("p");
+    for (let index = 0; index < parrafos.length; index++) {
+        parrafos[index].addEventListener("mouseenter", function (e) {
+            let reloj = document.getElementById("reloj");
+            reloj.style.display = "block";
+            let d = new Date();
+            reloj.innerHTML = (d.getHours()) + ":" + (d.getMinutes()) + ":" + (d.getSeconds());
+        });
+
+        parrafos[index].addEventListener("mousemove", function (e) {
+            let reloj = document.getElementById("reloj");
+            reloj.style.left = (e.clientX + 5) + "px";
+            reloj.style.top = (e.clientY - 5) + "px";
+        });
+
+        parrafos[index].addEventListener("mouseleave", function (e) {
+            let reloj = document.getElementById("reloj");
+            reloj.style.display = "none";
+            console.log(e.target, e.currentTarget);
+        });
+    }
 }
-       
     
 
